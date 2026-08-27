67738d992b3731f211736ea6767dac2316164ddb
unknown <m73349922@gmail.com>
2026-08-27 17:58:33 +0500

    wet

---
+++ WebApi/Program.cs
@@ -0,0 +1,21 @@
+﻿using Domain;
+
+User user = new User()
+{
+    Id = 2,
+    Name = "Muhammad",
+    Email = "134"
+};
+
+UserService service = new UserService();
+service.CreatUser(user);
+service.UpdateUser(user);
+service.DeleteUser(1);
+var get = service.GetAllUser();
+
+foreach (var item in get)
+{
+    Console.WriteLine($"Id: {item.Id}");
+    Console.WriteLine($"Name: {item.Name}");
+    Console.WriteLine($"Email: {item.Email}");
+}
